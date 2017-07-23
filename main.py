ac2fe5584635a7afba4e477986a0852907b5b59b
Florian Unger <florian.unger@fau.de>
2017-07-23 21:21:47 +0200

    added optional flag: controller_architecture

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ batch_size = 100
 
 parser = argparse.ArgumentParser()
 parser.add_argument("modelType", help="the kind of model you want to test, either ntm, dense or lstm")
-parser.add_argument("--epochs", help="the number of epochs to train", default="1000", type=int)
+parser.add_argument("-e", "--epochs", help="the number of epochs to train", default="1000", type=int)
+parser.add_argument("-c", "--ntm_controller_architecture", help="lstm or dense or gru, ignored if model is not ntm", default="dense")
 args = parser.parse_args()
 modelType = args.modelType
 epochs = args.epochs
+ntm_controller_architecture = args.ntm_controller_architecture
 
 
 if modelType == 'lstm':
@@ -22,7 +24,8 @@ if modelType == 'lstm':
     model = model_lstm.gen_model(input_dim=input_dim, output_dim=output_dim, batch_size=batch_size)
 elif modelType == 'ntm':
     import model_ntm
-    model = model_ntm.gen_model(input_dim=input_dim, output_dim=output_dim, batch_size=batch_size)
+    model = model_ntm.gen_model(input_dim=input_dim, output_dim=output_dim, batch_size=batch_size,
+                                    controller_architecture=ntm_controller_architecture)
 elif modelType == 'dense':
     import model_dense
     model = model_dense.gen_model(input_dim=input_dim, output_dim=output_dim, batch_size=batch_size)
@@ -30,7 +33,7 @@ else:
     raise ValueError("this model is not implemented yet")
 
 print("model built, starting the copy experiment")
-
+import pudb; pu.db
 from testing_utils import lengthy_test
 lengthy_test(model, epochs=epochs)
 
